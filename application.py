4d0a6c9fb50231c90f38c1729b4dd96bb04781e0
17736581 <jacksonwhitford@gmail.com>
2022-04-06 19:15:43 +1000

    changed route and html tempalte names, added proper search query, fleshed out add form

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
-#Set up environment variable
+#Set up environment variables
 #$env:FLASK_APP = "application"
+#$env:FLASK_ENV = "development"
 
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+if __name__ == "__main__":
+    app.run(debug=True)
 
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = ""
@@ -24,25 +27,27 @@ def hello_world():
 
 # route to search projects
 # todo: finalise query to input form data
-@app.route("/projects", methods=["GET", "POST"])
-def projects():
+@app.route("/search", methods=["GET", "POST"])
+def search():
     if request.method == "GET":
-        return render_template("projects.html")
+        return render_template("search.html")
     
     if request.method == "POST":
         cursor = mysql.connection.cursor()
         author = request.form.get("author")
-        query = """SELECT authors.first_name, authors.last_name, projects.project_name FROM project_authors
+        query = """SELECT projects.project_name, GROUP_CONCAT(CONCAT(' ', authors.first_name, ' ', authors.last_name)) authors
+                FROM project_authors
                 INNER JOIN projects ON projects.project_id = project_authors.project_id
                 INNER JOIN authors ON authors.author_id = project_authors.author_id
                 WHERE project_authors.project_id IN
-	                (SELECT project_authors.project_id FROM project_authors 
-	                INNER JOIN authors ON authors.author_id = project_authors.author_id 
-	                INNER JOIN projects ON projects.project_id = project_authors.project_id 
-	                WHERE first_name = 'Ante' AND last_name = 'Prodan');"""
+	            (SELECT project_authors.project_id FROM project_authors 
+	            INNER JOIN authors ON authors.author_id = project_authors.author_id 
+	            INNER JOIN projects ON projects.project_id = project_authors.project_id 
+	            WHERE first_name = 'Ante' AND last_name = 'Prodan')
+                GROUP BY projects.project_name"""
         cursor.execute(query)
         projects = cursor.fetchall()
-        return render_template("get_projects.html", projects=projects)
+        return render_template("search_results.html", projects=projects)
 
 # route to add projects
 @app.route("/add", methods=["GET", "POST"])
@@ -51,18 +56,17 @@ def add():
         return render_template("add.html")
     
     if request.method == "POST":
-        form_data = request.form
+        form_data = request.form.to_dict()
         cursor = mysql.connection.cursor()
         print(form_data)
         query = """INSERT INTO projects(project_name, url, start_date, end_date, release_date, country, funding_org, funding_amount) 
         VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"""
-        for i in form_data:
-            for j, k in i:
-                if k == "":
-                    k = None
+        for k in form_data:
+            if form_data[k] == '':
+                form_data[k] = None
         print(form_data)
         cursor.execute(query, [form_data["project_name"], form_data["url"], form_data["start_date"], form_data["end_date"], form_data["release_date"], form_data["country"], form_data["funding_org"], form_data["funding_amount"]])
-    #    mysql.connection.commit()
+        mysql.connection.commit()
         return render_template("added.html", form_data=form_data)
 
     
